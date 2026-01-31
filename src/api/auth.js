7181874d394d7f60b9cc35775a0c718e3b77450d
shrutirapolu <155569386+rapoluShruti@users.noreply.github.com>
2026-01-31 11:13:04 +0530

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api/auth";
+const API_BASE = "https://inventory-backend-oayq.onrender.com";
 
 export const tokenUtils = {
   getToken: () => localStorage.getItem("token"),
